167c7df28b80147232f681c142d0f112873c89bf
Mehmet ali Çakmak <79807829+mehmet5643@users.noreply.github.com>
2022-07-23 18:53:54 +0300

    change updated

--- hardhat/lock-project/contracts/lock.sol
+++ hardhat/lock-project/contracts/lock.sol
@@ -8,20 +8,22 @@ contract Lock{
     uint256 public totalLocked;
 
     mapping(address =>uint256) public lockers;
+    mapping(address =>uint256) public deadline;
 
     constructor(address tokenAddress){
         Token=MASIToken(tokenAddress);
     }
 
-    function lockTokens(uint256 amount) external {
+    function lockTokens(uint256 amount,uint time) external {
         require(amount >0,"Token amount must be bigger than 0.");
-
+        require(time >0,"Time must be bigger than 0.");
         // require(Token.balanceOf(msg.sender) >= amount,"Insufficent balance.");
         // require(Token.allowance(msg.sender,address(this)) >= amount,"Insufficent allowance.");
 
         if (lockers[msg.sender] <0) lockerCount++;
         totalLocked+=amount;
         lockers[msg.sender]+=amount;
+        deadline[msg.sender]=block.timestamp+time;
 
         bool ok = Token.transferFrom(msg.sender,address(this),amount);
         require(ok,"Transfer failed.");
@@ -30,9 +32,10 @@ contract Lock{
 
     function withdrawToken() external {
         require(lockers[msg.sender] >0,"Not enough token.");
-
+        require(deadline[msg.sender] >= block.timestamp,"Time out.");
         uint256 amount = lockers[msg.sender];
         delete(lockers[msg.sender]);
+        delete(deadline[msg.sender]);
         totalLocked -=amount;
         lockerCount--;
         
--- hardhat/lock-project/hardhat.config.js
+++ hardhat/lock-project/hardhat.config.js
@@ -1,22 +1,20 @@
 //require("@nomiclabs/hardhat-ethers");
 require("@nomiclabs/hardhat-waffle");
 require('dotenv').config();
-const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const PRIVATE_KEY = "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80";
 
 
 module.exports = {
     solidity: "0.8.2",
     networks: {
-      hardhat:{
-        url :process.env.ALCHEMY_URL,
-      },
+      
       mainnet: {
         url: `https://api.avax.network/ext/bc/C/rpc`,
-        accounts: [`${PRIVATE_KEY}`]
+   accounts: [`${PRIVATE_KEY}`]
       },
       fuji: {
         url: `https://api.avax-test.network/ext/bc/C/rpc`,
-          accounts: [`${PRIVATE_KEY}`]
+      accounts: [`${PRIVATE_KEY}`]
       }
     }
 };
