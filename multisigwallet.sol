598dbf0495408aeed5ccc27b4fe18ff6cc8cc0c3
Mehmet Ali Çakmak <79807829+mehmet5643@users.noreply.github.com>
2023-09-14 10:20:35 +0300

    Create multisigwallet.sol

---
+++ multisigwallet.sol
@@ -0,0 +1,80 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+contract MultiSig {
+    address[] public owners;
+    uint public transactionCount;
+    uint public required;
+
+    struct Transaction {
+        address payable destination;
+        uint value;
+        bool executed;
+        bytes data;
+    }
+
+    mapping(uint => Transaction) public transactions;
+    mapping(uint => mapping(address => bool)) public confirmations;
+
+    receive() payable external {
+        
+    }
+
+    function executeTransaction(uint transactionId) public {
+        require(isConfirmed(transactionId));
+        Transaction storage _tx = transactions[transactionId];
+        (bool success, ) = _tx.destination.call{ value: _tx.value }(_tx.data);
+        require(success, "Failed to execute transaction");
+        _tx.executed = true;
+    }
+
+    function isConfirmed(uint transactionId) public view returns(bool) {
+        return getConfirmationsCount(transactionId) >= required;
+    }
+
+    function getConfirmationsCount(uint transactionId) public view returns(uint) {
+        uint count;
+        for(uint i = 0; i < owners.length; i++) {
+            if(confirmations[transactionId][owners[i]]) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    function isOwner(address addr) private view returns(bool) {
+        for(uint i = 0; i < owners.length; i++) {
+            if(owners[i] == addr) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    function submitTransaction(address payable dest, uint value, bytes memory data) external {
+        uint id = addTransaction(dest, value, data);
+        confirmTransaction(id);
+    }
+
+    function confirmTransaction(uint transactionId) public {
+        require(isOwner(msg.sender));
+        confirmations[transactionId][msg.sender] = true;
+        if(isConfirmed(transactionId)) {
+            executeTransaction(transactionId);
+        }
+    }
+
+    function addTransaction(address payable destination, uint value, bytes memory data) public returns(uint) {
+        transactions[transactionCount] = Transaction(destination, value, false, data);
+        transactionCount += 1;
+        return transactionCount - 1;
+    }
+
+    constructor(address[] memory _owners, uint _confirmations) {
+        require(_owners.length > 0);
+        require(_confirmations > 0);
+        require(_confirmations <= _owners.length);
+        owners = _owners;
+        required = _confirmations;
+    }
+}
